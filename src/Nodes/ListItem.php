7ad09f4c04fe15360853f1940e5d4dc77acf7c3f
kriskbx <m@kris.cool>
2019-02-07 12:26:48 +0100

    add paragraph wrapper to listitem

--- src/Nodes/ListItem.php
+++ src/Nodes/ListItem.php
@@ -11,6 +11,12 @@ class ListItem extends Node
 
     public function data()
     {
+        if (count($this->DOMNode->childNodes) > 1) {
+            $this->wrapper = [
+                'type' => 'paragraph',
+            ];
+        }
+
         return [
             'type' => 'list_item',
         ];
